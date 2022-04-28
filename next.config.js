3b0d1bbfb4c7d527c3eb8d14eff620a9ae293a95
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-04-27 19:44:46 -0700

    install dotenv

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+const webpack = require('webpack')
+const { parsed: myEnv } = require('dotenv').config({
+  path:'/env'
+})
 const nextConfig = {
   reactStrictMode: true,
   images: {
@@ -6,3 +10,9 @@ const nextConfig = {
   },
 };
 module.exports = nextConfig;
+module.exports = {
+  webpack(config) {
+      config.plugins.push(new webpack.EnvironmentPlugin(myEnv))
+      return config
+  }
+}
