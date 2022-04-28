4e3287c5b75af3bf6e15dc5c5f967419b4675974
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-04-28 16:54:02 -0700

    style update

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -47,7 +47,12 @@ const Featured = () => {
       >
         {images.map((item) => (
           <div className={styles.imgContainer} key={item.id}>
-            <div className={styles.description}>{item.desc}</div>
+            <div
+              className={styles.desc}
+              style={{ fontFamily: "Square Peg" }}
+            >
+              {item.desc}
+            </div>
             <Image src={item.image} alt="" layout="fill" objectFit="cover" />
           </div>
         ))}
