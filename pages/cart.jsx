83a51f507a7ce440d213a29d4252f1504041f765
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-05-02 10:46:27 -0700

    style update

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -24,7 +24,7 @@ const Cart = () => {
 
   const createOrder = async (data) => {
     try {
-      const res = await axios.post(`http://cafe-order-app.herokuapp.com/api/orders`, data);
+      const res = await axios.post(`https://cafe-order-app.herokuapp.com/api/orders`, data);
       // if (res.status === 201) {
       //   dispatch(reset());
       //   router.push(`/orders/${res.data._id}`);
